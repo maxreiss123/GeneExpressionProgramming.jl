79dddb68362ed702a88a788a0e4be697b8d7afde
Maximilian Reißmann <reissmannm@4000l-217946-l.mobility.unimelb.net.au>
2025-02-15 10:39:13 +1100

    reset param

--- src/RegressionWrapper.jl
+++ src/RegressionWrapper.jl
@@ -263,20 +263,20 @@ Dictionary containing default probabilities and parameters for genetic algorithm
 These values can be adjusted to fine-tune the genetic algorithm's behavior.
 """
 const GENE_COMMON_PROBS = Dict{String,AbstractFloat}(
-    "one_point_cross_over_prob" => 0.4,
-    "two_point_cross_over_prob" => 0.3,
+    "one_point_cross_over_prob" => 0.5,
+    "two_point_cross_over_prob" => 0.4,
     "mutation_prob" => 0.9,
-    "mutation_rate" => 0.05,
+    "mutation_rate" => 0.1,
     "dominant_fusion_prob" => 0.1,
-    "dominant_fusion_rate" => 0.2,
+    "dominant_fusion_rate" => 0.1,
     "rezessiv_fusion_prob" => 0.1,
-    "rezessiv_fusion_rate" => 0.2,
+    "rezessiv_fusion_rate" => 0.1,
     "fusion_prob" => 0.1,
-    "fusion_rate" => 0.2,
+    "fusion_rate" => 0.1,
     "inversion_prob" => 0.1,
-    "reverse_insertion" => 0.05,
-    "reverse_insertion_tail" => 0.05,
-    "mating_size" => 0.5)
+    "reverse_insertion" => 0.1,
+    "reverse_insertion_tail" => 0.1,
+    "mating_size" => 0.7)
 
 const SymbolDict = OrderedDict{Int8,Int8}
 const CallbackDict = Dict{Int8,Function}
